0dabbad4022649f4bd485b6d13138b53104cc7d0
lucholeonel <carrenoluciano@gmail.com>
2025-03-11 15:41:50 -0300

    fix project structure

---
+++ README.md
@@ -0,0 +1,54 @@
+# 🤖 SAMI
+
+🧪 Game where users have to chat and discover who is the AI agent, which disguises as a human on every game. Over time, the adaptive AI aims to be unrecognizable.
+
+⚙️ Built using Typescript, Eliza, NextJS, Express, Supabase, RainbowKit, Foundry, Wagmi and Viem.
+
+## Requirements
+
+Before you begin, you need to install the following tools:
+
+- [Git](https://git-scm.com/downloads)
+- [Docker](https://docs.docker.com/get-started/get-docker/)
+
+
+## Start with Docker
+
+1. Clone this repo:
+
+```
+git clone https://github.com/fabian416/sami.git
+cd sami
+```
+
+2. Duplicate the .env.example template from the ROOT and fill the variable PRIVATE_KEY and OPENAI_API_KEY with your own keys
+
+```bash
+cp .env.example .env
+```
+
+```
+PRIVATE_KEY= #Your private key  
+OPENAI_API_KEY=sk-* # OpenAI API key, starting with sk-  
+```
+
+3. Start docker
+
+```bash
+docker compose up -d
+```
+
+4. Enter to http://localhost:3001 and play with sami!
+
+## You can play online with SAMI at:
+
+- [https://playsami.fun](https://playsami.fun) _(Right-click → Open in new tab)_
+
+
+## Architecture
+
+- Frontend homepage at `packages/nextjs/app/page.tsx`.
+- Backend in `packages/backend`
+- AI in `packages/ai/sami`
+- Smart contracts in `packages/foundry/contracts`
+- Deployment scripts in `packages/foundry/script`
---
+++ packages/nextjs/app/layout.tsx
@@ -0,0 +1,42 @@
+import { SocketProvider } from "./socketContext";
+import "@rainbow-me/rainbowkit/styles.css";
+import { ScaffoldEthAppWithProviders } from "~~/components/ScaffoldEthAppWithProviders";
+import { ThemeProvider } from "~~/components/ThemeProvider";
+import "~~/styles/globals.css";
+import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
+
+export const metadata = getMetadata({
+  title: "SAMI - Play against AI",
+  description: "Bet on who do you think is the AI and earn a profit.",
+  imageRelativePath: "/thumbnail.jpg",
+});
+
+const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html suppressHydrationWarning>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Audiowide&family=Exo+2:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body>
+        <ThemeProvider enableSystem>
+          <ScaffoldEthAppWithProviders>
+            <SocketProvider>{children}</SocketProvider>
+          </ScaffoldEthAppWithProviders>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+};
+
+export default ScaffoldEthApp;
